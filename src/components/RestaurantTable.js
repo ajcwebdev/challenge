cdc71f297e3074cff2be810c02d66cc16ce1ee81
ajcwebdev <ac@ac.local>
2020-11-17 05:22:07 -0700

    Create Restaurant Table component

---
+++ src/components/RestaurantTable.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+const RestaurantTable = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, [])
+
+  const loadData = async () => {
+    const res = await fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Api-Key ' + process.env.REACT_APP_SECRET_KEY
+      }
+    })
+    const data = await res.json()
+    setRestaurants(data)
+  }
+
+  return (
+    <>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>City</th>
+          <th>State</th>
+          <th>Genre</th>
+          <th>Telephone</th>
+        </tr>
+
+        {restaurants.map(restaurant => {
+          return (
+            <tr key={restaurant.id}>
+              <td>{restaurant.name}</td>
+              <td>{restaurant.city}</td>
+              <td>{restaurant.state}</td>
+              <td>{restaurant.genre}</td>
+              <td>{restaurant.telephone}</td>
+            </tr>
+          )})}
+      </table>
+    </>
+  );
+}
+
+export default RestaurantTable;
