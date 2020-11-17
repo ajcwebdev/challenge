1de9e8b24bf1d75e4ab2d8fc82a634c37448e14c
ajcwebdev <ac@ac.local>
2020-11-17 05:25:42 -0700

    Import Restaurant Table component into root App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import RestaurantTable from "./components/RestaurantTable";
+
 const App = () => {
   return (
     <>
-      <h1>Restaurants</h1> 
+      <h1>Restaurants</h1>
+      <RestaurantTable />
     </>
   );
 }
